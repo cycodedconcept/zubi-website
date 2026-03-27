0bc90649d956f0308dd4b18a5b52e5c8dd84e85e
cycodedconcept <cycodedconcept@gmail.com>
2026-03-27 15:37:51 +0100

    footer is setting up the footer

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './component/fixed/Navbar';
+import Footer from './component/fixed/Footer';
 
 const Display = () => {
   return (
-    <></>
+    <>
+     <Navbar />
+     <Footer />
+    </>
   )
 }
 
